404a3a70e17ca906844b0986d9d659008d2b9234
Lee Sager <dleesager1@gmail.com>
2023-10-23 15:52:11 -0400

    add chat-ui-kit-react to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,42 @@
-import ChatWindow from "./components/ChatWindow";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+
 import { SocketProvider } from "./contexts/SocketContext";
 import "./App.css";
 function App() {
   return (
-    <SocketProvider>
-      <ChatWindow />
-    </SocketProvider>
+    <div style={{ position: "relative", height: "500px" }}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hi, how are you?",
+                sentTime: "2 min ago",
+                sender: "John Doe",
+                direction: "incoming",
+                position: "normal",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hi, how are you?",
+                sentTime: "1 min ago",
+                sender: "leewiz",
+                direction: "outgoing",
+                position: "normal",
+              }}
+            />
+          </MessageList>
+          <MessageInput placeholder="type chat here..." />
+        </ChatContainer>
+      </MainContainer>
+    </div>
   );
 }
 
